1d4be0955ff384953a6da84400fd6eef4ee2178e
Van (RRG) <van@rrgcre.com>
2026-08-08 13:23:19 -0500

    aaa

--- public/rrg_shell.js
+++ public/rrg_shell.js
@@ -112,6 +112,7 @@
     ] },
     { grp: 'Business Sales', color: '#6bbf95', items: [
       { ic: '⊞', label: 'Listings', href: 'rrg_board.html' },
+      { ic: '▤', label: 'Marketplace', href: 'rrg_marketplace.html' },
       { ic: '▥', label: 'Data Rooms', href: 'rrg_rooms_queue.html' },
       { ic: '◈', label: 'Deals', href: 'rrg_deals.html' }
     ] },
