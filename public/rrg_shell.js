97518629e7f69933239fda2058dec900ef4e2d98
Van (RRG) <van@rrgcre.com>
2026-08-12 20:55:35 -0500

    aaa

--- public/rrg_shell.js
+++ public/rrg_shell.js
@@ -192,6 +192,7 @@
       { ic: '➤', label: 'Market Attack Plans', href: 'rrg_attack_queue.html' },
       { ic: '▤', label: 'Marketplace', href: 'rrg_marketplace.html', admin: true },
       { ic: '✎', label: 'Email Templates', href: 'rrg_email_templates.html' },
+      { ic: '⇶', label: 'Mass Emails', href: 'rrg_mass_emails.html' },
       { ic: '◉', label: 'Tracked Emails', href: 'rrg_tracked_emails.html' },
       { ic: '⟳', label: 'Automations', href: 'rrg_admin_automations.html', admin: true }
     ] },
